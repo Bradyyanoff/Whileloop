ec1d981cd8f9a45065f4800af7ba6b59fe7c96ac
Brady Yanoff <113472249+Bradyyanoff@users.noreply.github.com>
2022-10-27 11:57:41 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+books = ["Python Crash Course", "Game Programming", "Code Complete", "Clean Code", "Python Crash Course",
+         "The Pragmatic Programmer", "Python Crash Course"]
+#while "Python Crash Course" in books:
+    #books.remove("Python Crash Course")
+
+#for location in range(len(books)):
+#    if books[location] == "Python Crash Course":
+#        books.pop(location)
+
+print(books)
+
+
